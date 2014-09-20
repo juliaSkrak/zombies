65f1298dba0cbb181ed1c1f86daa678ce9779db8
Shardul Baral <shardul.baral@mail.mcgill.ca>
2014-09-20 11:02:30 -0400

    fixed router to call controller properly. also added jade file for main page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
