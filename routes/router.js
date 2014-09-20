d89132d3c6647e869ad7ae80e9e16c81f33dd391
Shardul Baral <shardul.baral@mail.mcgill.ca>
2014-09-20 19:30:07 -0400

    empty

--- routes/router.js
+++ routes/router.js
@@ -46,7 +46,7 @@ module.exports = function(app){
                 isInfected: infected,
                 count: infectCount
               };
-              res.send(infectJSON);
+              res.json(infectJSON);
             }
           });
         }
