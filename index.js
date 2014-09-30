cd45c169a3c5f69c92b678467716b1372b715c20
Shardul Baral <shardul@abe.home>
2014-09-30 00:52:27 -0400

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+//this is a comment
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
